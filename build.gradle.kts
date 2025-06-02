7d59e58fcb011049c2f3242d9e247b6247e70f8b
17480 <beate.undeberget@mattilsynet.no>
2025-06-02 19:50:59 +0200

    PLANTE-150: Fjernet avhengighet til org.springframework.cloud:spring-cloud-dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,7 +62,6 @@ dependencies {
     implementation("com.google.protobuf:protobuf-java")
 
     // gcp
-    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2024.0.1"))
     implementation(platform("com.google.cloud:spring-cloud-gcp-dependencies:6.2.1"))
     implementation("com.google.cloud:spring-cloud-gcp-starter-secretmanager")
     implementation("com.google.cloud:spring-cloud-gcp-starter-storage")
