096d23e0f2705e2ee3659a277df1594e5ce463e3
Beate Undeberget <beate.undeberget@mattilsynet.no>
2026-07-02 09:01:18 +0200

    PLANTE-1888: Lagt til hubDeliveryNumber som subject på ephyto_import_envelopes_failed_v1 (#267)

--- src/main/kotlin/no/mattilsynet/ephyto/api/nats/jetstream/subjects/JetStreamSubjectBuilder.kt
+++ src/main/kotlin/no/mattilsynet/ephyto/api/nats/jetstream/subjects/JetStreamSubjectBuilder.kt
@@ -8,7 +8,10 @@ object JetStreamSubjectBuilder {
     fun ephytoImportEnvelopesV1() =
         JetStreamSubject("mattilsynet.ephyto.import.envelopes.v1", "ephyto_import_envelopes_v1")
 
-    fun ephytoImportEnvelopesFailedV1() =
-        JetStreamSubject("mattilsynet.ephyto.import.envelopes.failed.v1", "ephyto_import_envelopes_failed_v1")
+    fun ephytoImportEnvelopesFailedV1(hubDeliveryNumber: String) =
+        JetStreamSubject(
+            stream = "ephyto_import_envelopes_failed_v1",
+            subject = "mattilsynet.ephyto.import.envelopes.failed.v1.$hubDeliveryNumber",
+        )
 
 }
